6a27d94f0bac821a7e367d4aec165dabac520778
Hérick <herickcwilke@gmail.com>
2022-05-24 12:54:15 -0300

    Fix: removidas folders de desenvolvimento

--- backend-api/api/models/transacao.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Transacao = void 0;
-class Transacao {
-}
-exports.Transacao = Transacao;
--- backend-api/server.js
+++
@@ -1,7 +0,0 @@
-const app = require("./config/express")();
-const port = app.get("port");
-
-// Aplicativo ouvindo na porta definida em ./config/config.json
-app.listen(port, () => {
-  console.log(`Servidor rodando na porta ${port}`);
-});
